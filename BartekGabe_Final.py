b8edcd4b9629b167c695e4b8c6f90b81144f8dfe
GabeEarl <96506563+GabeEarl@users.noreply.github.com>
2022-12-04 21:26:44 -0600

    Add files via upload

--- BartekGabe_Final.py
+++ BartekGabe_Final.py
@@ -1,68 +1,80 @@
 import tkinter as tk
-import os
 
 root = tk.Tk()
+root.geometry("1000x1000")
+
+Inp1 = []
+Inp2 = []
 
 font1 = ('Arial', 22)
 font2 = ('Arial', 18)
 
-canvas = tk.Canvas(root, height=800, width=800, bg='#68F088')
-canvas.pack()
+# Window that holds everything
+canvas = tk.Canvas(root, bg='#68F088')
+canvas.place(relwidth=1,relheight=1)
 
+# Smaller window for color offset
 frame = tk.Frame(root, bg="#22B835")
 frame.place(relwidth=0.95, relheight=0.95, relx=0.025, rely=0.025)
 
+# Applications name
 Title = tk.Label(frame, text='Money Manager', font=font1, )
 Title.grid(column=1,row=0, padx=10, pady=10)
 
-Weekly = tk.Label(frame,text='Enter monthly earnings: ', font=font2)
-Weekly.grid(column=0, row=1, padx=10, pady=10)
+# function for taking the for loop entry data and subtracting monthly earnings from
+# tried to do input validation but can only get it to work for the last entry
+def total_Expense():
+    total = 0
+    cnun = 0
+    for entries in Inp1:
+        try:
+            int(entries.get())
+            total = total + int(entries.get())
+            total_num.config(text=total)
+        except ValueError:
+            total_num.config(text='Not a number')
+    try:
+        int(entries.get())
+        leftover = int(e1.get()) - total
+        gross_num.config(text=leftover)
+    except ValueError:
+        total_num.config(text='Not a number')
+
+# prompts for the for loop to display as labels
+prompts = ('Enter monthly rent including utilities: ',
+           'Enter monthly car and health insurance: ', 'Enter price of gas each month: ',
+           'Grocery expenses each month: ', 'Phone bill each month: ',
+           'Pet expenses each month: ', 'Monthly Subscriptions: ',
+           'Monthly credit card payment: ')
+
+# for loop for labels and corresponding entry areas
+for i in range(len(prompts)):
+    Labels = tk.Label(frame, text=prompts[i], font=font2)
+    Labels.grid(column=0, row= i +2, padx=10, pady=10)
+    Inputs = tk.Entry(frame, font=('Arial', 14),)
+    Inputs.grid(column=1, row=i+2, padx=10, pady=10)
+    Inp1.append(Inputs)
+
+# label and entry for the income since it is used differently in the function
+earn_Label = tk.Label(frame,text='Enter monthly earnings: ', font=font2)
+earn_Label.grid(column=0,row=1, padx=10, pady=10)
 e1 = tk.Entry(frame, font=('Arial', 14),)
 e1.grid(column=1, row=1, padx=10, pady=10)
 
-Rent = tk.Label(frame,text='Enter monthly rent including utilities: ', font=font2)
-Rent.grid(column=0, row=2, padx=10, pady=10)
-e2 = tk.Entry(frame, font=('Arial', 14),)
-e2.grid(column=1, row=2, padx=10, pady=10)
+# Label to show all the expenses for the month
+total_Label = tk.Label(frame,text='Total monthly expenses: ', font=font2)
+total_Label.grid(column=0,row=10, padx=10, pady=10)
+total_num = tk.Label(frame,text='', font=font2,)
+total_num.grid(column=1,row=10, padx=10, pady=10)
 
-carIns = tk.Label(frame,text='Enter monthly car and health insurance: ', font=font2)
-carIns.grid(column=0, row=3, padx=10, pady=10)
-e3 = tk.Entry(frame, font=('Arial', 14),)
-e3.grid(column=1, row=3, padx=10, pady=10)
+# Label to show the income after expenses have been taken out
+gross_Label = tk.Label(frame,text='Monthly income after expenses: ', font=font2)
+gross_Label.grid(column=0,row=11, padx=10, pady=10)
+gross_num = tk.Label(frame,text='', font=font2)
+gross_num.grid(column=1,row=11, padx=10, pady=10)
 
-Gas = tk.Label(frame,text='Enter price of gas each month: ', font=font2)
-Gas.grid(column=0, row=4, padx=10, pady=10)
-e4 = tk.Entry(frame, font=('Arial', 14),)
-e4.grid(column=1, row=4, padx=10, pady=10)
-
-Grocery = tk.Label(frame,text='Grocery expenses each month: ', font=font2)
-Grocery.grid(column=0, row=5, padx=10, pady=10)
-e5 = tk.Entry(frame, font=('Arial', 14),)
-e5.grid(column=1, row=5, padx=10, pady=10)
-
-Phone = tk.Label(frame,text='Phone bill each month: ', font=font2)
-Phone.grid(column=0, row=6, padx=10, pady=10)
-e6 = tk.Entry(frame, font=('Arial', 14),)
-e6.grid(column=1, row=6, padx=10, pady=10)
-
-Pet = tk.Label(frame,text='Pet expenses each month: ', font=font2)
-Pet.grid(column=0, row=7, padx=10, pady=10)
-e7 = tk.Entry(frame, font=('Arial', 14),)
-e7.grid(column=1, row=7, padx=10, pady=10)
-
-Sub = tk.Label(frame,text='Monthly Subscriptions: ', font=font2)
-Sub.grid(column=0, row=8, padx=10, pady=10)
-e8 = tk.Entry(frame, font=('Arial', 14),)
-e8.grid(column=1, row=8, padx=10, pady=10)
-
-CC = tk.Label(frame,text='Monthly credit card payment: ', font=font2)
-CC.grid(column=0, row=9, padx=10, pady=10)
-e9 = tk.Entry(frame, font=('Arial', 14),)
-e9.grid(column=1, row=9, padx=10, pady=10)
-
-afterExp = tk.Label(frame,text='Total after expenses: ', font=font2)
-afterExp.grid(column=0, row=10, padx=10, pady=10)
-expNum = tk.Label(frame,text='', font=font2)
-expNum.grid(column=1, row=10, padx=10, pady=10)
+# Button to calculate everything
+Calc = tk.Button(frame, text='Calculate', font=font2, command=total_Expense)
+Calc.grid(column=0, row=12, padx=10, pady=10)
 
 root.mainloop()
